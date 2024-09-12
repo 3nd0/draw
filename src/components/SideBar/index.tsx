121649352619e2253a1b0720a893bbd24e34c439
wataru endo <w4.3nd0@watarunoMacBook-Air.local>
2024-09-12 15:10:38 +0900

    Change layout

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -2,7 +2,7 @@ import { HomeIcon } from '@heroicons/react/20/solid'
 
 const SideBar = () => {
   return (
-    <nav className="fixed top-0 left-0 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0">
+    <nav className="w-64 h-screen transition-transform -translate-x-full sm:translate-x-0">
       <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
         <ul className="space-y-2 font-medium">
           <li>
