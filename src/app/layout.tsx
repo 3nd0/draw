fb76697640e0bcbc3e68a14c830d7d1b24c5ad8f
wataru endo <w4.3nd0@watarunoMacBook-Air.local>
2024-09-12 16:14:38 +0900

    Modify code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,10 +16,12 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <div className="flex flex-row h-screen">
-          <div className='fixed top-0 left-0'>
+          <div className="fixed top-0 left-0">
             <SideBar />
           </div>
-          <div className='fixed top-0 left-64 w-screen h-screen pb-40'>{children}</div>
+          <div className="fixed top-0 left-64 w-screen h-screen pb-40">
+            {children}
+          </div>
         </div>
       </body>
     </html>
